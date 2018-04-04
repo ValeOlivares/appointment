3b286d292e78782dc3581ed1c5ddf5a9107deae4
ValeOlivares <valentinaolivares85@gmail.com>
2018-04-04 00:24:34 -0300

    desintalando dependencias innecesarias y actualizando botón de estado

--- src/data/Supervisora.js
+++ src/data/Supervisora.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import confirmada from './dataImg/confirmada.png';
+import gestionado from './dataImg/gestionado.png';
 
 
 //const url = 'https://patricia.davila.cl/data_clinic/json/supervisora/161988464';
@@ -33,7 +35,7 @@ export default class Supervisora extends Component {
           {this.state.allData.map((allData, index) => {
             return (
               <div className="allDataCont" key={index}>
-                <p> {allData.reserved_date} {allData.reserved_hour} <span>{allData.state}</span></p>
+                <p> {allData.reserved_date} {allData.reserved_hour} <span>{(allData.state == "confirmada")? <img src={confirmada}/> :  <img src={gestionado}/> }</span></p>
                 <p> {allData.atention} <span>{allData.probability}</span></p>
                 <p> <span>{allData.type_pacient}</span>  {allData.name_pacient}</p>
                 <p> {allData.professional_name} <br/>
