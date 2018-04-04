0c64b29f11ede1309917a188deadaba0e7785206
ValeOlivares <valentinaolivares85@gmail.com>
2018-04-04 01:48:16 -0300

    agregando grid

--- src/data/Supervisora.js
+++ src/data/Supervisora.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 import confirmada from './dataImg/confirmada.png';
 import gestionado from './dataImg/gestionado.png';
 
@@ -17,16 +18,16 @@ export default class supervisora extends Component {
   componentDidMount() {
     fetch(`http://patricia.davila.cl/data_clinic/json/supervisora/161988464`)
 
-    .then(function(response){
-      //console.log(response);
-      return response.json();
-    })
-    .then(response => {
-      // console.log(response) // This works
-      const allData = response.data;
-      console.log(allData);
-      this.setState({ allData });
-    })
+      .then(function (response) {
+        //console.log(response);
+        return response.json();
+      })
+      .then(response => {
+        // console.log(response) // This works
+        const allData = response.data;
+        console.log(allData);
+        this.setState({ allData });
+      })
   }
 
   render() {
@@ -35,13 +36,47 @@ export default class supervisora extends Component {
         <div>
           {this.state.allData.map((allData, index) => {
             return (
-              <div className="allDataCont" key={index}>
-                <p> {allData.reserved_date} {allData.reserved_hour} <span>{(allData.state == "confirmada")? <img src={confirmada}/> :  <img src={gestionado}/> }</span></p>
-                <p> {allData.atention} <span>{allData.probability}</span></p>
-                <p> <span>{allData.type_pacient}</span>  {allData.name_pacient}</p>
-                <p> {allData.professional_name} <br/>
-                {allData.professional_speciality}</p> <span>{allData.price}</span>
-              </div>
+              <Grid>
+                <div className="allDataCont" key={index}>
+                  <Row className="show-grid">
+                    <Col xs={8} >
+                      <p> {allData.reserved_date} {allData.reserved_hour} </p>
+                    </Col>
+                    <Col xs={4}>
+                      {(allData.state == "confirmada") ? <Image src={confirmada} responsive /> : <Image src={gestionado} responsive />}
+                    </Col>
+                  </Row>
+                  <Row className="show-grid">
+                    <Col xs={8}>
+                      <p> {allData.atention} </p>
+                    </Col>
+                    <Col xs={4}>
+                      <p>{allData.probability}</p>
+                    </Col>
+                  </Row>
+                  <Row className="show-grid">
+                    <Col xs={2}>
+                      <p>{allData.type_pacient}</p>
+                    </Col>
+                    <Col xs={10}>
+                      <p>{allData.name_pacient}</p>
+                    </Col>
+                  </Row>
+                  <Row className="show-grid">
+                    <Col xs={8}>
+                      <p> {allData.professional_name} </p>
+                    </Col>
+                  </Row>
+                  <Row className="show-grid">
+                    <Col xs={8}>
+                      <p>{allData.professional_speciality}</p>
+                    </Col>
+                    <Col xs={4}>
+                      <p>{allData.price}</p>
+                    </Col>
+                  </Row>
+                </div>
+              </Grid>
             )
           })}
         </div>
